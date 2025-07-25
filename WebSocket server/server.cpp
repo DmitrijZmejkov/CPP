d9766a1cffd5349cf35cbb1ee27ef05c47983257
Dmitrij_Zmejkov <120366668+DmitrijZmejkov@users.noreply.github.com>
2025-07-25 20:25:26 +0300

    Update server.cpp
    functions is added

--- WebSocket server/server.cpp
+++ WebSocket server/server.cpp
@@ -22,11 +22,7 @@ using tcp = boost::asio::ip::tcp;
 bool is_valid_ip(const std::string& ip);
 void logger(const std::string& message);
 void start_session(tcp::socket socket, int time_period);
-
-bool is_valid_ip(const std::string& ip);
-void logger(const std::string& message);
-void start_session(tcp::socket socket, int time_period);
-
+std::string nowtime();
 
 int main() {
     int default_port = 5000;
@@ -82,9 +78,64 @@ int main() {
                 start_session(std::move(socket), time_period);
             }).detach();
         }
-            
     } catch (const std::exception& e) {
         logger("ERROR: " + std::string(e.what()));
         return 1;
     }
 }
+
+
+bool is_valid_ip(const std::string& ip) {
+    try {
+        net::ip::make_address(ip); 
+        return true; 
+    } catch (const std::exception&) {
+        return false;  
+    }
+}
+
+
+void start_session(tcp::socket socket, int time_period) {
+    try {
+        beast::websocket::stream<tcp::socket> ws{std::move(socket)};
+        ws.accept();
+        std::random_device rd;
+        std::mt19937 gen(rd());
+        std::uniform_real_distribution<> dis(0.0, 1.0);
+        while (true) {
+            double res = dis(gen);
+            ws.write(net::buffer(std::to_string(res)));
+            std::cout << "Sent: " << res << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(time_period));
+        }
+    } catch (beast::system_error const& se) {
+        if (se.code() != beast::websocket::error::closed) {
+            std::cout << "client disconected " << std::endl;
+            logger("WebSocket error: " + se.code().message());
+        }
+    } catch (std::exception const& e) {
+        std::cout << "Error !" << std::endl;
+        logger("Error: " + std::string(e.what()));
+    }
+}
+
+
+std::string nowtime() {
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time_t = std::chrono::system_clock::to_time_t(now);
+    std::tm now_tm = *std::localtime(&now_time_t);
+    std::ostringstream oss; 
+    oss << std::put_time(&now_tm, "%H:%M:%S %Y-%m-%d"); 
+    return oss.str();
+}
+
+
+void logger(const std::string& message) {
+    std::ofstream log_file("logg_file.log", std::ios_base::app);
+    log_file << message << " " << nowtime() << std::endl;
+}
+
+
+
+
+
